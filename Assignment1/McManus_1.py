ea8e087f2a1f938b13ddff835371db396a96062d
Kevin McManus <kevin.leo.mcmanus@gmail.com>
2022-01-11 11:46:31 -0700

    First Assignments

---
+++ Assignment1/McManus_1.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Assignment \#1
+# ## Kevin McManus, student id: 109702479
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# In[13]:
+
+
+def MyPlot(x,y,title, my_ax=None):
+    if my_ax is None:
+        fig, ax = plt.subplots( figsize=(8,6))
+    else:
+        ax = my_ax
+        
+    ax.plot(x,y)
+    ax.set_title(title)
+    ax.set_xlabel('Variable X'); ax.set_ylabel('Variable Y')
+    return ax
+        
+def MyErrorPlot(x,y,title, yerror, my_ax=None):
+    if my_ax is None:
+        fig, ax = plt.subplots( figsize=(8,6))
+    else:
+        ax = my_ax
+
+    ax.errorbar(x,y, yerr=yerror)
+    ax.set_title(title)
+    ax.set_xlabel('Variable X'); ax.set_ylabel('Variable Y')
+    return ax
+    
+
+"""
+# In[16]:
+
+
+xx = np.linspace(0,4*np.pi, 60)
+yy = np.cos(xx)
+title_str='Cosine Function'
+
+MyPlot(xx,yy, title_str)
+
+
+# In[17]:
+
+
+yerror = yy*0.2 # error is 20% of the value
+ax = MyErrorPlot(xx,yy,  title_str+' (with Error Bars)', yerror=yerror)
+ax.grid(axis='y')
+
+
+# In[ ]:
+"""
+
+
+
